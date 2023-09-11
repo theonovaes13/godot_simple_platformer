13124bc6097ffb0f860221aa8b9bf6363abfcc0a
Theo Novaes <theonovaes13@hotmail.com>
2023-09-11 20:22:19 -0300

    add coins, fix tileset

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -21,15 +21,25 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	var jump = Input.is_action_just_pressed("ui_accept") and is_on_floor()
+	if jump:
 		velocity.y = JUMP_INITIAL_VELOCITY
+		
+	if !is_on_floor():
+		animation.set_animation('jump')
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if !direction and !jump and is_on_floor():
+		animation.set_animation('idle')
+	
 	if direction:
 		animation.flip_h = true if direction < 0 else false
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			animation.set_animation('running')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,14 +1,26 @@
 extends Node2D
-
+@onready var coin: Area2D = $"."
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_box: CollisionShape2D = $CollisionShape2D
+@onready var coin_pickup_fx: AudioStreamPlayer = $coin_pickup_fx
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func coin_pickup() -> void:
+	animation.set_animation('pickup_sfx')
+	coin_pickup_fx.play()
+
+func is_pickupable() -> bool:
+	return animation.get_animation() == 'idle'
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if collision_box.shape.collide(Transform2D(), collision_box.shape, Transform2D()):
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if is_pickupable():
+		if body.is_in_group('playerScene'):
+			coin_pickup()
 	
