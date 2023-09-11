66206c6355ee3f437beb83def710618c0c53aeb8
Theo Novaes <theonovaes13@hotmail.com>
2023-09-11 10:17:51 -0300

    WIP: basic scenery, add coin stub

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_INITIAL_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_INITIAL_VELOCITY = -320.0
+const VIEWPORT_SIZE = 640
+@onready var animation = $animation
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Get the animation sprite size to clamp the character's position.
+var playerSize =  Vector2(32,32)
+var leftBorder = 0 + playerSize.x / 2
+var rightBorder = VIEWPORT_SIZE - playerSize.x / 2
 
 func _physics_process(delta):
+	global_position.x = clamp(global_position.x, leftBorder, rightBorder)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -21,8 +28,11 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		animation.flip_h = true if direction < 0 else false
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# Move the character.	
 	move_and_slide()
+	
