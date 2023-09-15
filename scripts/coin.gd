ccc3d75680c3150aaa9eb6d4f6b408db3a8fe512
Theo Novaes <theonovaes13@hotmail.com>
2023-09-15 02:11:50 -0300

    chore: fix warnings

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -17,7 +17,7 @@ func is_pickupable() -> bool:
 	return animation.get_animation() == 'idle'
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
