b882300c19d8dbcaa8e003ff4db64aa7baa945ab
Theo Novaes <theonovaes13@hotmail.com>
2023-09-15 02:11:18 -0300

    feat: add moving platform

--- scripts/moving_platform.gd
+++ scripts/moving_platform.gd
@@ -3,26 +3,23 @@ extends Node2D
 const WAIT_DURATION = 1.0
 
 @onready var platform: AnimatableBody2D = $platform
-@export var move_speed := 3.0
-@export var move_distance := 192
-@export var horizontal := true
+@export var movement_duration := 2
+@export var horizontal_distance := 192
+@export var vertical_distance := 192
+@export var horizontal_movement := true
+@export var vertical_movement := true
 
 var follow := Vector2.ZERO
 var platform_center := 16
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start_tween()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
-	platform.position = platform.position.lerp(follow, 0.5)
-
-func move_platform() -> void:
-	var move_direction = Vector2.RIGHT if horizontal else Vector2.UP
-	var duration = move_direction.length() / float(move_speed * platform_center)
-
-	var platform_tween = create_tween().set_loops().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
-	platform_tween.tween_property(self, 'follow', move_direction, duration)
+func start_tween():
+	var x = horizontal_distance if horizontal_movement else 0
+	var y = vertical_distance if vertical_movement else 0
+	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.set_loops().set_parallel(false)
+	tween.tween_property(platform, "position", Vector2(x,y),movement_duration)
+	tween.tween_property(platform, "position", Vector2.ZERO,movement_duration)
 
